29f354940024be3834bc0fcc6e60d7978ad7c2da
Wasif97488 <w4wasifrock@gmail.com>
2026-01-13 19:09:41 +0530

    DataTypes

---
+++ Lec04_13_Jan/Datatype1.java
@@ -0,0 +1,48 @@
+class Datatype1
+{
+public static void main(String args[])
+{
+byte b = 127;
+//byte c = 128;
+//byte d = -129;
+//short s = 32768;
+//short s1 = -32769;
+
+//int datatype
+int i = -32769;
+int i1 = 6534533;
+int i2 = 65345;
+
+
+//long datatype
+long l = 512373;
+long l1 = 624534637;
+long l2 = 9748862437l;
+long l3 =9748862437l;
+//long l4 = 736387338382.383l;
+
+
+//float datatype
+//float f = 123.456;
+float f1 = 123.456f;
+float f2 = 123456.789f;
+float f3 = 12345.6789f;
+float f4 = 12876.0923f;
+float f5 = 98765.123f;
+float f6 = 1234567.89753f;
+float f7 = 12345678.54123f;
+
+double d = 12345678.54123d;
+
+
+System.out.println(f2);
+System.out.println(f3);
+System.out.println(f4);
+System.out.println(f5);
+System.out.println(f6);
+System.out.println(f7);
+System.out.println(d);
+
+
+}
+}
