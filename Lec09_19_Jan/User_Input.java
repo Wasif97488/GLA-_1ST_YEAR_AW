91c316e8aef6f7e9f7e0f76b44712ef3568914f2
Wasif97488 <w4wasifrock@gmail.com>
2026-01-20 14:40:27 +0530

    user_input

---
+++ Lec09_19_Jan/User_Input.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class User_Input
+{
+public static void main(String args[])
+{
+Scanner wasif = new Scanner(System.in);
+System.out.println("Enter the First Number");
+int a =wasif.nextInt();
+System.out.println("Enter the Second Number");
+int b= wasif.nextInt();
+System.out.println("Enter the Third Number");
+double c=wasif.nextDouble();
+System.out.println("Enter the float Number");
+float f = wasif.nextFloat();
+
+
+
+
+System.out.println("The First Number is "+a);
+System.out.println("The Second Number is "+b);
+System.out.println("The Third Number is "+c);
+System.out.println("The float value are "+f);
+
+
+
+}
+}
+
