84830a1768689d891dd63b3afa2aa6069d51e07f
Wasif97488 <w4wasifrock@gmail.com>
2026-03-24 11:51:36 +0530

    Linear Search

---
+++ Lec38_23_Mar/LastSearchElement.java
@@ -0,0 +1,42 @@
+package Lec38_23_Mar;
+
+import java.util.Scanner;
+
+public class LastSearchElement {
+	public static void main(String[] args) {
+		
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter the Size of Array");
+	    int size = sc.nextInt();
+	    int[] arr = new int[size];
+	    
+	    System.out.println("Enter the element in an array");
+	    for(int i=0;i<size;i++)
+	    {
+	    	arr[i]=sc.nextInt();
+	    }
+	    
+	    System.out.println("Enter the element to be search:");
+	    int eleSear = sc.nextInt();
+	    
+//	    int count=0;
+	    boolean flag=false;
+	    int i=size-1;
+	    for(;i>=0;i--)
+	    {
+	    	if(arr[i]==eleSear)
+	    	{
+//	    		count++;
+	    		flag=true;
+	    		break;
+	    	}
+	    }
+	    
+	    if(flag==true)
+	    	System.out.println(i);
+	    else
+	    	System.out.println(-1);
+		}
+
+
+}
